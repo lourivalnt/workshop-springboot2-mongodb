5a0ef0e66c8b00a0b477758d119a8957d9f04e16
Lourival Linard <l.linard@gmail.com>
2019-02-11 09:19:22 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/spring/workshopmongo/WorkshopSpringboot2MongodbApplication.java
@@ -0,0 +1,14 @@
+package com.spring.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringboot2MongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringboot2MongodbApplication.class, args);
+	}
+
+}
+
