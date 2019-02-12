241c74f856ac915b8c1e283f7e1ee16b829ca957
Lourival Linard <l.linard@gmail.com>
2019-02-12 09:09:18 -0300

    Referenciando os posts do usuário

--- src/main/java/com/spring/workshopmongo/services/UserService.java
+++ src/main/java/com/spring/workshopmongo/services/UserService.java
@@ -29,12 +29,12 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
+
 	public User update(User obj) {
 		User newObj = findById(obj.getId());
 		updateData(newObj, obj);
